88a9be37fc2f91303b792d3f9e2c8a21c630f7c7
Valentina-Peralta <125395224+Valentina-Peralta@users.noreply.github.com>
2023-08-17 19:03:56 -0300

    set options colors when selected

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ function App() {
   const [actualQuestion, setActualQuestion] = useState()
   const [actualOptions, setActualOptions] = useState([])
   const [correct, setCorrect] = useState(false)
+  const [correctIndex, setCorrectIndex] = useState(-1); // Inicialmente no se selecciona ninguna opción
+  const [incorrectIndex, setIncorrectIndex] = useState(-1); // Inicialmente no se selecciona ninguna opción
+
   const fetchData = async () => {
     try {
       const response = await fetch('https://opentdb.com/api.php?amount=10&type=multiple');
@@ -46,22 +49,36 @@ function App() {
           <div className="options">
             {actualOptions.map((option, index) =>
               <button
+                className={(correct === true && correctIndex === index) || (option === actualQuestion.correct_answer && correct === false && correctIndex === -1 && incorrectIndex != -1) ? 'correct' : correct === false && incorrectIndex === index ? 'incorrect' : null}
                 key={index}
                 onClick={() => {
                   if (option === actualQuestion.correct_answer) {
                     console.log('correct')
+                    setCorrect(true)
+                    setCorrectIndex(index);
+
+                  } else {
+                    setIncorrectIndex(index)
+                    console.log(index, correctIndex, incorrectIndex)
+
                   }
                 }}
               >{option}</button>
             )}
           </div>
           <button
-            onClick={() => setQuestionNum(num => num + 1)}
+            onClick={() => {
+              setQuestionNum(num => num + 1)
+              setCorrect(false)
+              setCorrectIndex(-1)
+              setIncorrectIndex(-1)
+            }}
           >
             Next question</button>
-        </div> : null}
+        </div> : null
+      }
 
-    </div>
+    </div >
   )
 }
 
