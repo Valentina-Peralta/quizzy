b257aa2a8f8eef43685dc9816f5412ad92e4582d
Valentina-Peralta <125395224+Valentina-Peralta@users.noreply.github.com>
2023-08-18 00:25:18 -0300

    score counter, restart button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import Categories from './components/Categories'
 
 function App() {
   const [questions, setQuestions] = useState([])
@@ -9,7 +10,9 @@ function App() {
   const [correct, setCorrect] = useState(false)
   const [correctIndex, setCorrectIndex] = useState(-1); // Inicialmente no se selecciona ninguna opción
   const [incorrectIndex, setIncorrectIndex] = useState(-1); // Inicialmente no se selecciona ninguna opción
-  const [category, setCategory] = useState()
+  const [category, setCategory] = useState('')
+  const [score, setScore] = useState(0)
+
   const fetchData = async () => {
     try {
       const response = await fetch(`https://opentdb.com/api.php?amount=10&type=multiple${category}`);
@@ -40,85 +43,58 @@ function App() {
   return (
     <div className="app">
       <div className="questions_options">
-        {!category && <div className="category">
-          <h3>Select category</h3>
-          <ul>
-            <li
-              onClick={() => setCategory('')}
-            >Any</li>
-            <li
-              onClick={() => setCategory('&category=9')}
-            >General knowledge</li>
-            <li
-              onClick={() => setCategory('&category=17')}
-
-            >Science & Nature</li>
-            <li
-              onClick={() => setCategory('&category=18')}
-
-            >Computers</li>
-            <li
-              onClick={() => setCategory('&category=19')}
-
-            >Mathematics</li>
-            <li
-              onClick={() => setCategory('&category=21')}
-            >Sports</li>
-            <li
-              onClick={() => setCategory('&category=22')}
-
-            >Geography</li>
-            <li
-              onClick={() => setCategory('&category=23')}
-
-            >History</li>
-            <li
-              onClick={() => setCategory('&category=25')}
-
-            >Arts</li>
-            <li
-              onClick={() => setCategory('&category=27')}
-
-            >Animals</li>
-
-          </ul>
-        </div>}
+        {!category && <Categories
+          setCategory={setCategory} />}
 
       </div>
       {category && questions.length > 0 ?
-        <div className="question_wrapper">
-          <p>Question {questionNum}/10</p>
-          <p>{questions[questionNum - 1].question}</p>
+        <main>
+          <h2>Score: {score}</h2>
+          <div className="question_wrapper">
+            <p>Question {questionNum}/10</p>
+            <p>{questions[questionNum - 1].question}</p>
 
-          <div className="options">
-            {actualOptions.map((option, index) =>
+            <div className="options">
+              {actualOptions.map((option, index) =>
+                <button
+                  className={(correct === true && correctIndex === index) || (option === actualQuestion.correct_answer && correct === false && correctIndex === -1 && incorrectIndex != -1) ? 'correct' : correct === false && incorrectIndex === index ? 'incorrect' : null}
+                  key={index}
+                  onClick={() => {
+                    if (option === actualQuestion.correct_answer) {
+                      setCorrect(true)
+                      setCorrectIndex(index);
+                      setScore(prev => prev + 1)
+
+                    } else {
+                      setIncorrectIndex(index)
+                      console.log(index, correctIndex, incorrectIndex)
+
+                    }
+                  }}
+                >{option}</button>
+              )}
+            </div>
+            {questionNum < 10 ?
               <button
-                className={(correct === true && correctIndex === index) || (option === actualQuestion.correct_answer && correct === false && correctIndex === -1 && incorrectIndex != -1) ? 'correct' : correct === false && incorrectIndex === index ? 'incorrect' : null}
-                key={index}
                 onClick={() => {
-                  if (option === actualQuestion.correct_answer) {
-                    setCorrect(true)
-                    setCorrectIndex(index);
+                  setCorrect(false)
+                  setCorrectIndex(-1)
+                  setIncorrectIndex(-1)
+                  setQuestionNum(num => num + 1)
 
-                  } else {
-                    setIncorrectIndex(index)
-                    console.log(index, correctIndex, incorrectIndex)
-
-                  }
                 }}
-              >{option}</button>
-            )}
+              >
+                Next question</button>
+              :
+              <button
+                onClick={() => {
+                  fetchData()
+                  setQuestionNum(1)
+                }}
+              >Restart</button>
+            }
           </div>
-          <button
-            onClick={() => {
-              setQuestionNum(num => num + 1)
-              setCorrect(false)
-              setCorrectIndex(-1)
-              setIncorrectIndex(-1)
-            }}
-          >
-            Next question</button>
-        </div> : null
+        </main> : null
       }
 
     </div >
---
+++ src/components/Categories.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Categories = ({ setCategory }) => {
+    return (
+        <div className="category">
+            <h3>Select category</h3>
+            <ul className='categories'>
+                <li
+                    onClick={() => setCategory(' ')}
+                >Any</li>
+                <li
+                    onClick={() => setCategory('&category=9')}
+                >General knowledge</li>
+                <li
+                    onClick={() => setCategory('&category=17')}
+
+                >Science & Nature</li>
+                <li
+                    onClick={() => setCategory('&category=18')}
+
+                >Computers</li>
+                <li
+                    onClick={() => setCategory('&category=19')}
+
+                >Mathematics</li>
+                <li
+                    onClick={() => setCategory('&category=21')}
+                >Sports</li>
+                <li
+                    onClick={() => setCategory('&category=22')}
+
+                >Geography</li>
+                <li
+                    onClick={() => setCategory('&category=23')}
+
+                >History</li>
+                <li
+                    onClick={() => setCategory('&category=25')}
+
+                >Arts</li>
+                <li
+                    onClick={() => setCategory('&category=27')}
+
+                >Animals</li>
+
+            </ul>
+        </div>)
+}
+
+export default Categories
